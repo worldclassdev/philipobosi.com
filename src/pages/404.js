0e085325c693a0976c4b342abc68224a19681517
worldclassdev <philip.obosi@eng.uniben.edu>
2020-04-25 21:50:46 +0100

    fix: page layout import

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,13 @@
+import { PageLayout } from "../components/layouts"
 import React from "react"
-
-import { HomeLayout } from "../components/layouts"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <HomeLayout>
+  <PageLayout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </HomeLayout>
+  </PageLayout>
 )
 
 export default NotFoundPage
