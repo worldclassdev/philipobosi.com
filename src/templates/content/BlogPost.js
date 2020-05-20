769135fcda2c81b1d67068c34766314f033f8fe7
worldclassdev <philip.obosi@eng.uniben.edu>
2020-05-20 16:20:15 +0100

    updates: add post detail query

--- src/templates/content/BlogPost.js
+++ src/templates/content/BlogPost.js
@@ -3,7 +3,7 @@
 import PageLayout from "../../components/layouts/PageLayout"
 import React from "react"
 import SEO from "../../components/seo"
-// import { graphql } from "gatsby"
+import { graphql } from "gatsby"
 import styles from "./content.module.scss"
 
 class BlogPost extends React.Component {
@@ -41,30 +41,30 @@ class BlogPost extends React.Component {
 
 export default BlogPost
 
-// export const pageQuery = graphql`
-//   query BlogPostBySlug($slug: String!) {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//     markdownRemark(fields: { slug: { eq: $slug } }) {
-//       id
-//       excerpt(pruneLength: 160)
-//       html
-//       frontmatter {
-//         title
-//         date(formatString: "MMMM DD, YYYY")
-//         description
-//         author {
-//           name
-//           bio
-//           profilePicture
-//           social {
-//             twitter
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        description
+        author {
+          name
+          bio
+          profilePicture
+          social {
+            twitter
+          }
+        }
+      }
+    }
+  }
+`
