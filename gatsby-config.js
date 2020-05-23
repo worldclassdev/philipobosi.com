211598a103ea60e439d60430c33ebc0e6997f491
worldclassdev <philip.obosi@eng.uniben.edu>
2020-05-23 13:57:41 +0100

    feature: blog rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,7 @@ module.exports = {
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-feed`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
