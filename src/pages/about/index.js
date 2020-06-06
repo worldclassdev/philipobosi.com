1c96fce3ab36301ceaf2e3e1432c3efdfe44a1ff
worldclassdev <philip.obosi@eng.uniben.edu>
2020-06-06 03:54:45 +0100

    about page updates

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,12 +2,13 @@ import { NavMenu } from "../../components/home"
 import { PageLayout } from "../../layouts"
 import React from "react"
 import styles from "./about.module.scss"
+import ProfileImage from "../../assets/images/profile-avatar.jpg"
 
 const AboutPage = () => (
   <PageLayout>
     <section className={styles.about}>
       <header className={styles.about_header}>
-        <div className={styles.about_header_image}></div>
+        <img src={ProfileImage} className={styles.about_header_image} />
         <h1 className={styles.about_header_title}>
           Frontend Engineer and Product Designer from Lagos, Nigeria.
         </h1>
@@ -24,7 +25,46 @@ const AboutPage = () => (
           philip@getcreathor.com
         </a>
       </header>
-      <div className={styles.about_content}></div>
+      <div className={styles.about_content}>
+        <p>
+          I am currently focused on creating high quality content around
+          fundamental and advanced web development topics and concepts in the
+          form of blogs, articles, talks and video tutorials.
+        </p>
+        <p>
+          You may find some of my articles on{" "}
+          <a href="https://scotch.io/search?q=worldclassdev" target="_blank">
+            Scotch.io
+          </a>
+          ,{" "}
+          <a
+            href="https://blog.logrocket.com/author/philipobosi/"
+            target="_blank"
+          >
+            LogRocket
+          </a>
+          , and{" "}
+          <a href="https://www.twilio.com/blog/author/pobosi" target="_blank">
+            Twilio.
+          </a>
+        </p>
+        <p>
+          Previously, I architected and led frontend engineering efforts at{" "}
+          <a href="https://kudi.com" rel="canonical">
+            Kudi Inc.
+          </a>{" "}
+          where I contributed to an internal component design system as well as
+          built several admin tools(dashboards and bots) for the smooth
+          operation of the business.
+        </p>
+        <p>
+          Before Kudi, I founded{" "}
+          <a href="https://shopable.com" rel="canonical">
+            Shopable
+          </a>{" "}
+          ( a mobile app for selling on multiple social media platforms.).
+        </p>
+      </div>
     </section>
   </PageLayout>
 )
